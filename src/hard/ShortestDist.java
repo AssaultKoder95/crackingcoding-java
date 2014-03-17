8cd655ea1ce73d36b649d1f48e0e9187360b4a99
Nan Zhang <zhangnan2920214@gmail.com>
2014-03-16 20:53:30 -0700

    Quick Sort related Operation

---
+++ src/hard/ShortestDist.java
@@ -0,0 +1,24 @@
+package hard;
+
+public class ShortestDist {
+    public static int solution(String[] text, String word1, String word2) {
+        int pos1 = -1, pos2 = -1, min = Integer.MAX_VALUE;
+        for (int i = 0; i < text.length; i++) {
+            if (text[i].equals(word1)) {
+                pos1 = i;
+                if(pos2 != -1){
+                    min = Math.min(min, pos1 - pos2);
+                }
+            } else if (text[i].equals(word2)) {
+                pos2 = i;
+                if(pos1 != -1){
+                    min = Math.min(min, pos2 - pos1);
+                }
+            }
+        }
+        return min;
+    }
+    public static void main(String[] args) {
+        System.out.println(ShortestDist.solution(new String[]{"aa", "bb", "cc", "dd", "aa", "cc"}, "aa",  "cc"));
+    }
+}
